5bafb5357028f05c43f689938b52a85e71e6fe12
Sabyrzhan Zhaslanuly <verosabyrka@gmail.com>
2026-09-03 09:53:45 +0600

    chore: extend Button component with isActive and isDisabled props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx'
 import styles from './Button.module.scss'
 
-export const Button = ({ className, children }) => {
+export const Button = ({ className, children, isActive, isDisabled }) => {
   return (
     <button
-      className={clsx(styles.button, className)}
-      type="button"
+      className={clsx(styles.button, isActive && styles.active, className)}
+      type="button" disabled={isDisabled}
     >
       {children}
     </button>
