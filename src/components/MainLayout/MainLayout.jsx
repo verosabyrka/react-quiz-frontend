35aff732ff30fb789780b118ad05be1f6b289e87
Sabyrzhan Zhaslanuly <verosabyrka@gmail.com>
2026-09-03 06:54:45 +0600

    chore: replace inline footer with Footer component in MainLayout

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,20 +1,13 @@
+import { Footer } from '../Footer'
 import styles from './MainLayout.module.scss'
 
 export const MainLayout = () => {
-  const currentYear = new Date().getFullYear()
-
   return (
     <div className={styles.mainLayout}>
       <header>header</header>
       <div className={styles.mainWrapper}>
         <main className={styles.main}>content</main>
-        <footer className={styles.footer}>
-          React Question Cards Application<br />
-          © <time dateTime={currentYear.toString()}>{currentYear}</time> Built by&nbsp;
-          <a href="https://github.com/verosabyrka" target="_blank" rel="noopener noreferrer">
-            Sabyrka
-          </a>.
-        </footer>
+        <Footer />
       </div>
     </div>
   )
