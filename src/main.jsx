2571f90be009f30e07e03fcd5722755bb01d5db7
Sabyrzhan Zhaslanuly <verosabyrka@gmail.com>
2026-09-03 06:03:44 +0600

    chore: reorder and import @a1rth/css-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+import '@a1rth/css-normalize'
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { App } from './App'
 
 createRoot(document.getElementById('root')).render(
