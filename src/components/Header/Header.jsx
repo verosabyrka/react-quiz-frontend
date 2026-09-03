be780b2da6f0c9bacd78252c12281bd3241f9d43
Sabyrzhan Zhaslanuly <verosabyrka@gmail.com>
2026-09-03 08:34:33 +0600

    chore: replace div with semantic header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import styles from './Header.module.scss'
 
 export const Header = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <Link className={styles.headerLogo} to="/">ReactQuiz</Link>
       <div className={styles.headerActions}>
         <Button>New card</Button>
         <Button>Log in</Button>
       </div>
-    </div>
+    </header>
   )
 }
